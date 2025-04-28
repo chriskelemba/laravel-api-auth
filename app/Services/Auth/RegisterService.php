09da78227168af5597e75ce65742896816adcf30
chriskelemba <loverhxchris@gmail.com>
2025-04-28 10:48:11 +0300

    duplicate email error handling

--- app/Services/Auth/RegisterService.php
+++ app/Services/Auth/RegisterService.php
@@ -4,11 +4,18 @@ namespace App\Services\Auth;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class RegisterService
 {
     public function execute(array $data)
     {
+        if (User::where('email', $data['email'])->exists()) {
+            throw ValidationException::withMessages([
+                'email' => ['Email already exists.']
+            ]);
+        }
+        
         $data['password'] = Hash::make($data['password']);
         return User::create($data);
     }
